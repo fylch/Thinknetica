c4b9d11c8e80d41fe181df9e1ec3c8a39751893a
fylch <drumsfan@gmail.com>
2019-11-20 13:10:23 +0200

    Update weight.rb

--- Lesson1/weight.rb
+++ Lesson1/weight.rb
@@ -4,7 +4,7 @@ name = gets.chomp
 puts 'Tell me your height, please: '
 height = gets.chomp.to_i
 
-ideal = height - 110
+ideal = (height - 110) * 1.15
 
 if ideal >= 0
   puts "Hello, #{name}. Your ideal weight is #{ideal}"
